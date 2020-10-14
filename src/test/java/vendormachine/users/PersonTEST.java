24d723f2d9b22bb09001dafa024a11517b57f15a
Elizabeth Lewis <elewis@academytrainee.com>
2020-10-14 14:39:21 +0100

    Updating naming, testing structure to be more reliable

--- src/test/java/vendormachine/users/PersonTEST.java
+++ src/test/java/vendormachine/users/PersonTEST.java
@@ -1,35 +1,34 @@
 package vendormachine.users;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Ignore;
-import org.junit.Test;
-import java.lang.StringBuilder;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
-import vendormachine.users.Person;
 import vendormachine.users.util.Wallet;
 
 // Example 100% Person Test Coverage
 // add @Ignore above class to help WalletTest.java
-public class PersonTEST {
-	
+public class PersonTEST {	
 	// Resources
+	private Person person;
 	private float credit = 10.0f;
-	private static Wallet testWallet = new Wallet();
+	private Wallet testWallet;
+	private final String name = "Alice";
 	
 	// Keep track of tests
 	private static int activeTest = 1;
 	private static StringBuilder sBuilder = new StringBuilder();
 	private static String div = "=============================================\n";
 	
-	@Before
-	public void beforeTest() {
-		
-		// Set Default test Wallet credit
-		testWallet.setCredit(credit);
+	@BeforeEach
+	public void init() {
+		// Set-up resources
+		this.testWallet = new Wallet();
+		this.testWallet.setCredit(this.credit);
+		this.person = new Person(this.name, this.testWallet);
 		
 		// Start of console test format
 		sBuilder.setLength(0);
@@ -55,60 +54,66 @@ public class PersonTEST {
 		assertTrue(allan instanceof Person);
 	}
 	
+	// Getter & Setter tests
 	@Test
-	public void test_setgetName() {
-		Person alan = new Person("Allan");
-		
-		alan.setName("Alan");
-		
-		assertEquals("Alan", alan.getName());
+	void setNameTest() {
+		String newName = "Alan";
+		this.person.setName(newName);
+		assertEquals(newName, this.person.getName());
 	}
 	
 	@Test
-	public void test_setgetWallet() {
-		Person alan = new Person("Allan");
-		
-		alan.setWallet(testWallet);
-		
-		assertTrue(alan.getWallet() instanceof Wallet);
+	void getNameTest() {
+		assertEquals(this.name, this.person.getName());
 	}
 	
 	@Test
-	public void test_getCredit() {
-		Person alyx = new Person("Alyx", testWallet);
-		float result = alyx.getCredit(5.0f);
+	void setGetWalletTest() {
+		Wallet newTestWallet = new Wallet();
+		this.person.setWallet(newTestWallet);
+		assertEquals(newTestWallet, this.person.getWallet());
+	}
+	
+	@Test
+	void getWalletTest() {
+		assertEquals(this.testWallet, this.person.getWallet());
+	}
+	
+	@Test
+	public void getCreditTest() {
+		float result = this.person.getCredit(5.0f);
 		
 		assertEquals(5.0f, result, 0.1f);
 	}
 	
 	@Test
 	public void test_addCredit() {
-		Person alyx = new Person("Alyx", testWallet);
-		alyx.addCredit(5.0f);
+		this.person.addCredit(5.0f);
 		
-		assertEquals(15.0f, alyx.getCredit(15.0f), 0.1f);
+		assertEquals(15.0f, this.person.getCredit(15.0f), 0.1f);
 	}
 	
 	@Test
 	public void test_noWallet() {
+		// Generate example 'person' with no wallet
 		Person alyx = new Person("Alyx");
 		
+		// Try to add credit to the person's wallet
 		alyx.addCredit(10.0f);
 		
+		// Assert they have no wallet
+		// And assert no credit can be drawn from the nonexistant wallet
 		assertTrue(alyx.getWallet() == null);
 		assertEquals(0, alyx.getCredit(5.0f), 0.1f);
 	}
 	
 	@Test
-	public void test_insufficentWalletFunds() {
-		Person alyx = new Person("Alyx", testWallet);
-		
-		assertEquals(0f, alyx.getCredit(20.0f), 0.1f);
+	public void insufficientWalletFundsTest() {
+		assertEquals(0f, this.person.getCredit(20.0f), 0.1f);
 	}
 	
-	@After
+	@AfterEach
 	public void afterTest() {
 		System.out.println(div);
 	}
-
 }
